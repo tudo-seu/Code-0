bff222c8efa59eddb8af65290d5da3f632c9bd2a
tudo-seu <hannes.2001@gmx.de>
2023-05-06 00:13:49 +0200

    Update hannes.py

--- notebooks/hannes.py
+++ notebooks/hannes.py
@@ -14,13 +14,27 @@ df.sort_values(by = "Measure")
 #df.dropna()     #deleting NaN Values
 
 f = df.loc[:, 'Measure'].unique()
+print(f)
 
 
+'''
 for i in f:
     result = df[df['Measure'] == i]
-    result = result[result.index % 4 != 0]
-    sns.relplot(x='time', y='kWh', data=result, kind='line')
+    result = result[result.index % 4 != 0]      #drop every 4th line
+    #sns.relplot(x='time', y='kWh', data=result, kind='line')
 
-    plt.show()
+    # plt.show()
     t = input()
-print(len(f))
+    
+'''
+
+curr = df[df['Measure'] == 'FB3 - Main 3L']
+
+### Generating Test and Training sets
+size = len(curr)
+
+test_indices = np.random.choice(size, (size*0.2), replace=False)
+all_indices = np.arange(size)
+train_indices = np.setdiff1d(all_indices, test_indices)
+
+
