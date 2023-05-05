3a851cc8cc12062b4bd8b122187a12ca9a75265e
tudo-seu <hannes.2001@gmx.de>
2023-05-05 23:03:49 +0200

    Updated Hannes.py

--- notebooks/hannes.py
+++ notebooks/hannes.py
@@ -1,8 +1,14 @@
 import pandas as pd
 import numpy as np
+import  os
 
-data = 'train_data.csv'
+os.chdir("..")
+
+data = 'data/train_data.csv'
 df = pd.read_csv(data)
 
 df.sort_values(by = "Measure")
+
+df.dropna()     #deleting NaN Values
+
 print(df.head())
