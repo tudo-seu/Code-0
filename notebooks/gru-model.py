0167b2e12ac9a1334e0aa6178893e9f72ce63bec
tudo-seu <hannes.2001@gmx.de>
2023-05-06 03:25:07 +0200

    Updated GRU

--- notebooks/gru-model.py
+++ notebooks/gru-model.py
@@ -3,37 +3,50 @@ from keras.layers import GRU, Dense
 from keras.callbacks import EarlyStopping
 import pandas as pd
 import numpy as np
-import  os
+import os
 import matplotlib.pyplot as plt
 import seaborn as sns
 os.chdir("..")
 
 data = 'data/train_data.csv'
 df = pd.read_csv(data)
-curr = df[df['Measure'] == 'FB3 - Main 3L']
+curr = df[df['Measure'] == 'Sub - Feeder F02']
+label_map = {'Non-production': 0, 'Power-down': 1, 'Power-up': 2, 'Production': 3}
 
 # Format curr
 size = len(curr)
-curr = curr.drop(columns=['Measure', 'label'])
+print(curr.head())
+curr = curr.dropna()
+curr = curr.drop(columns=['Measure'])
 curr = curr.reset_index(drop=True)
+
+
+curr['map'] = curr['label'].map(label_map)
 
 # Create Data sets
 test_indices = np.random.choice(size, (int(size*0.2)), replace=False)
 all_indices = np.arange(size)
 train_indices = np.setdiff1d(all_indices, test_indices)
+train = curr.iloc[:-(curr.shape[0] % 96)]
 
-X_train = curr.loc[train_indices, ('')]
 
+
+x_train = train.loc[train_indices]['kWh'].values.reshape(-1, 96, 1)
+y_train = train.loc[train_indices]['map'].values.reshape(-1, 1)
+
+x_val = curr.loc[test_indices, ('kWh')].values.reshape(-1, 96, 1)
+y_val = curr.loc[test_indices, ('map')].values.reshape(-1, 1)
+
+print(x_train)
 ### Generating Test and Training sets
-size = len(curr)
-curr = curr.drop(columns=['Measure', 'label'])
-curr = curr.reset_index(drop=True)
+
+
 
 model = Sequential()
 model.add(GRU(units=32, input_shape=(96, 1)))
 model.add(Dense(units=4, activation='softmax'))
 
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'], run_eagerly=True)
 
 early_stopping = EarlyStopping(monitor='val_loss', patience=3, mode='min')
-model.fit(X_train, Y_train, epochs=50, batch_size=32, validation_data=(X_val, y_val), callbacks=[early_stopping])
+model.fit(x_train, y_train, epochs=50, batch_size=32, validation_data=(x_val, y_val), callbacks=[early_stopping])
